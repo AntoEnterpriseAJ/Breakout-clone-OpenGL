0f788b5d378b512c739309fa8b9f98b5fa80608b
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-09-10 17:39:16 +0300

    added scene rendering

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,23 +1,76 @@
 #include "Game.h"
 #include "ResourceManager.h"
+#include "GLFW/glfw3.h"
 #include "glm/glm.hpp"
+#include <chrono>
 
-Game::Game(unsigned int width, unsigned int height)
-    : m_width{width}, m_height{height}
-{
-    init();
-}
+static float deltaTime           = 0.0f;
+static float lastTime            = 0.0f;
+static glm::vec2 paddleSize      = {100.0f, 20.0f};
+static glm::vec2 paddlePosition  = {static_cast<float>(800) / 2.0f - paddleSize.x / 2.0f, static_cast<float>(600) - paddleSize.y};
+static float paddleVelocity      = 500.0f;
+
+Game::Game(GLFWwindow* window,unsigned int width, unsigned int height)
+    : m_window{window}, m_width{width}, m_height{height}, m_currentLevel{0}, m_state{GAME_ACTIVE}
+{}
 
 void Game::render()
 {
-    m_spriteRenderer->drawSprite(ResourceManager::getTexture("awesomeFace"), glm::vec2(200.0f, 200.0f),
-                                 glm::vec2(300.0f, 400.0f), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+    auto currentTime = glfwGetTime();
+    deltaTime = currentTime - lastTime;
+    lastTime  = currentTime;
+
+    processInput();
+    if (m_state == GAME_ACTIVE)
+    {
+        const Texture2D& background = ResourceManager::getTexture("background");
+        m_spriteRenderer->drawSprite(background, {0.0f, 0.0f}, {m_width, m_height}, 0.0f);
+
+        m_levels[m_currentLevel].draw(m_spriteRenderer);
+
+        playerPaddle->draw(m_spriteRenderer);
+    }
+}
+
+void Game::processInput()
+{
+    if (glfwGetKey(m_window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(m_window, GLFW_TRUE);
+
+    float velocity = paddleVelocity * deltaTime;
+
+    if (glfwGetKey(m_window, GLFW_KEY_A) == GLFW_PRESS || glfwGetKey(m_window, GLFW_KEY_LEFT == GLFW_PRESS))
+    {
+        playerPaddle->getPositionRef() += glm::vec2{-1.0f, 0.0f} * velocity;
+    }
+    else if (glfwGetKey(m_window, GLFW_KEY_D) == GLFW_PRESS || glfwGetKey(m_window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+    {
+        playerPaddle->getPositionRef() += glm::vec2{1.0f, 0.0f} * velocity;
+    }
 }
 
 void Game::init()
 {
     ResourceManager::loadTexture("res/textures/awesomeface.png", "awesomeFace");
+    ResourceManager::loadTexture("res/textures/background.jpg", "background");
+    ResourceManager::loadTexture("res/textures/block_solid.png", "block_solid");
+    ResourceManager::loadTexture("res/textures/block.png", "block");
+    ResourceManager::loadTexture("res/textures/paddle.png", "paddle");
+
     ResourceManager::loadShader("res/shaders/sprite.vert", "res/shaders/sprite.frag", "spriteShader");
+
+    GameLevel level1; level1.loadFromFile("res/levels/lvl1.txt", m_height / 2, m_width);
+    GameLevel level2; level2.loadFromFile("res/levels/lvl2.txt", m_height / 2, m_width);
+    GameLevel level3; level3.loadFromFile("res/levels/lvl3.txt", m_height / 2, m_width);
+    GameLevel level4; level4.loadFromFile("res/levels/lvl4.txt", m_height / 2, m_width);
+
+    m_levels.push_back(level1);
+    m_levels.push_back(level2);
+    m_levels.push_back(level3);
+    m_levels.push_back(level4);
+
+    playerPaddle = std::make_unique<GameObject>(ResourceManager::getTexture("paddle"), paddlePosition,
+                                                paddleSize, glm::vec3{1.0f, 1.0f, 1.0f}, true);
 
     m_spriteRenderer = std::make_unique<SpriteRenderer>(ResourceManager::getShader("spriteShader"));
 
@@ -27,9 +80,9 @@ void Game::init()
 
     auto orthographicProjection = [](float l, float r, float b, float t, float n, float f){
         glm::mat4 ortho = {
-            2.0f / (r - l)  , 0.0f           , 0.0f             , - (l + r) / (r - l),
+            2.0f / (r - l)  , 0.0f           , 0.0f             , - (r + l) / (r - l),
             0.0f            , 2.0f / (t - b) , 0.0f             , - (t + b) / (t - b),
-            0.0f            , 0.0f           , 2.0f / (f - n)   ,   (f + n) / (f - n),
+            0.0f            , 0.0f           , - 2.0f / (f - n) , - (f + n) / (f - n),
             0.0f            , 0.0f           , 0.0f             , 1.0f,
         };
 
@@ -37,7 +90,7 @@ void Game::init()
     };
 
     glm::mat4 projection = orthographicProjection(0.0f, static_cast<float>(m_width), 
-                                      static_cast<float>(m_height), 0.0f, -1.0f, 1.0f);
+                                                  static_cast<float>(m_height), 0.0f, -1.0f, 1.0f);
 
     shader.setInt("spriteTexture", 0);
     shader.setMat4("projection", projection);
--- src/include/Game.h
+++ src/include/Game.h
@@ -1,25 +1,33 @@
 #pragma once
 #include "SpriteRenderer.h"
+#include "GLFW/glfw3.h"
+#include "GameLevel.h"
 #include <memory>
-
-enum GameState
-{
-	GAME_ACTIVE,
-	GAME_MENU,
-	GAME_WIN,
-};
 
 class Game
 {
 public:
-	Game(unsigned int width, unsigned int height);
+    enum GameState
+    {
+	    GAME_ACTIVE,
+	    GAME_MENU,
+	    GAME_WIN,
+    };
+
+	Game(GLFWwindow* window,unsigned int width, unsigned int height);
 
     void render();
 
-private:
     void init();
 private:
+    void processInput();
+private:
+    GLFWwindow* m_window;
 	GameState m_state;
 	unsigned int m_width, m_height;
+	unsigned int m_currentLevel;
+
     std::unique_ptr<SpriteRenderer> m_spriteRenderer;
+    std::vector<GameLevel> m_levels;
+    std::unique_ptr<GameObject> playerPaddle;
 };
