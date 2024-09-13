4f81f75100b0aef9c94a9f21ffa2e8830229a6d6
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-09-13 18:27:11 +0300

    fix

--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -1,12 +1,12 @@
 #include "GameObject.h"
 
 GameObject::GameObject()
-    : m_sprite{}, m_position{0.0f}, m_size{0.0f}, m_velocity{0.0f},
+    : m_sprite{}, m_position{0.0f}, m_size{0.0f},
       m_color{1.0f}, m_rotationAngle{0.0f}, m_isSolid{false}, m_isDestroyed{true} 
 {}
 
 GameObject::GameObject(const Texture2D& sprite, glm::vec2 pos, glm::vec2 size, glm::vec3 color, bool isSolid)
-    : m_sprite{sprite}, m_position{pos}, m_size{size}, m_velocity{0.0f},
+    : m_sprite{sprite}, m_position{pos}, m_size{size},
       m_color{color}, m_rotationAngle{0.0f}, m_isSolid{isSolid}, m_isDestroyed{false} 
 {}
 
