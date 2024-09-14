c534a6f73ca54bead25d27c55266cebee0100ecb
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-09-14 20:59:49 +0300

    getters for smoother interface

--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -2,12 +2,12 @@
 
 GameObject::GameObject()
     : m_sprite{}, m_position{0.0f}, m_size{0.0f},
-      m_color{1.0f}, m_rotationAngle{0.0f}, m_isSolid{false}, m_isDestroyed{true} 
+      m_color{1.0f}, m_rotationAngle{0.0f}, m_isBreakable{false}, m_isDestroyed{true} 
 {}
 
-GameObject::GameObject(const Texture2D& sprite, glm::vec2 pos, glm::vec2 size, glm::vec3 color, bool isSolid)
+GameObject::GameObject(const Texture2D& sprite, glm::vec2 pos, glm::vec2 size, glm::vec3 color, bool isBreakable)
     : m_sprite{sprite}, m_position{pos}, m_size{size},
-      m_color{color}, m_rotationAngle{0.0f}, m_isSolid{isSolid}, m_isDestroyed{false} 
+      m_color{color}, m_rotationAngle{0.0f}, m_isBreakable{isBreakable}, m_isDestroyed{false} 
 {}
 
 void GameObject::draw(const std::unique_ptr<SpriteRenderer>& spriteRenderer) const
@@ -15,7 +15,32 @@ void GameObject::draw(const std::unique_ptr<SpriteRenderer>& spriteRenderer) con
     spriteRenderer->drawSprite(m_sprite, m_position, m_size, m_rotationAngle, m_color);
 }
 
+glm::vec2 GameObject::getPosition() const
+{
+    return m_position;
+}
+
+glm::vec2 GameObject::getSize() const
+{
+    return m_size;
+}
+
+bool GameObject::isBreakable() const
+{
+    return m_isBreakable;
+}
+
+bool GameObject::isDestroyed() const
+{
+    return m_isDestroyed;
+}
+
 glm::vec2& GameObject::getPositionRef()
 {
     return m_position;
+}
+
+void GameObject::destroy()
+{
+    m_isDestroyed = true;
 }
--- src/include/GameObject.h
+++ src/include/GameObject.h
@@ -7,16 +7,22 @@ class GameObject
 {
 public:
     GameObject();
-    GameObject(const Texture2D& sprite, glm::vec2 pos, glm::vec2 size, glm::vec3 color, bool isSolid);
+    GameObject(const Texture2D& sprite, glm::vec2 pos, glm::vec2 size, glm::vec3 color, bool isBreakable);
 
     void draw(const std::unique_ptr<SpriteRenderer>& spriteRenderer) const;
+    glm::vec2 getPosition() const;
+    glm::vec2 getSize() const;
+    bool isBreakable() const;
+    bool isDestroyed() const;
+
     glm::vec2& getPositionRef();
+    void destroy();
 
 protected:
     glm::vec2 m_position, m_size;
     glm::vec3 m_color;
     float m_rotationAngle;
-    bool m_isSolid, m_isDestroyed;
+    bool m_isBreakable, m_isDestroyed;
 
     const Texture2D& m_sprite;
 };
