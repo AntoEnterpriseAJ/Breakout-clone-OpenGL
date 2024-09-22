afa4b63b7b16ee6ac93efe6df81acde43a74caa0
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-09-22 22:51:05 +0300

    cleaned the code a bit

--- src/Application.cpp
+++ src/Application.cpp
@@ -67,77 +67,6 @@ int main(void)
     //glfwSwapInterval(0); 
     glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-    // MSFBO:
-    unsigned int MSFBO;
-    glGenFramebuffers(1, &MSFBO);
-    glBindFramebuffer(GL_FRAMEBUFFER, MSFBO);
-
-    // MULTISAMPLED COLOR ATTACHMENT:
-    unsigned int textureColorbuffer;
-    glGenTextures(1, &textureColorbuffer);
-    glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, textureColorbuffer);
-
-    constexpr unsigned int samples = 4;
-    glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, samples, GL_RGB, SCREEN_WIDTH, SCREEN_HEIGHT, GL_TRUE);
-
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D_MULTISAMPLE, textureColorbuffer, 0);
-
-    // MULTISAMPLED DEPTH AND STENCIL RBO ATTACHMENT:
-    unsigned int MSRBO;
-    glGenRenderbuffers(1, &MSRBO);
-    glBindRenderbuffer(GL_RENDERBUFFER, MSRBO);
-    glRenderbufferStorageMultisample(GL_RENDERBUFFER, samples, GL_DEPTH24_STENCIL8, SCREEN_WIDTH, SCREEN_HEIGHT);
-    glBindRenderbuffer(GL_RENDERBUFFER, 0);
-
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT , GL_RENDERBUFFER, MSRBO);
-
-    if (glad_glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE)
-    {
-        std::cout << "Succesffuly created the MS FRAMEBUFFER" << std::endl;
-    }
-    else
-    {
-        std::cout << "WARNING: MS FRAMEBUFFER creation failed\n" << std::endl;
-        exit(-1);
-    }
-
-    // INTERMEDIARY FBO
-    unsigned int intermediaryFBO;
-    glGenFramebuffers(1, &intermediaryFBO);
-    glBindFramebuffer(GL_FRAMEBUFFER, intermediaryFBO);
-
-    // COLOR ATTACHMENT
-    unsigned int intermediaryColorBuffer;
-    glGenTextures(1, &intermediaryColorBuffer);
-    glBindTexture(GL_TEXTURE_2D, intermediaryColorBuffer);
-
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCREEN_WIDTH, SCREEN_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, intermediaryColorBuffer, 0);
-
-    // DEPTH AND STENCIL RBO
-    unsigned int intermediaryRBO;
-    glGenRenderbuffers(1, &intermediaryRBO);
-    glBindRenderbuffer(GL_RENDERBUFFER, intermediaryRBO);
-
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, SCREEN_WIDTH, SCREEN_HEIGHT);
-    glBindRenderbuffer(GL_RENDERBUFFER, 0);
-
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, intermediaryRBO);
-
-    if (glad_glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE)
-    {
-        std::cout << "Succesffuly created the Intermediary Framebuffer" << std::endl;
-    }
-    else
-    {
-        std::cout << "WARNING: Intermediary Framebuffer creation failed\n" << std::endl;
-        exit(-1);
-    }
-
     // Generate grid vertices
     std::vector<float> gridVertices;
     int gridSpacing = 5;  // 1-pixel spacing grid
@@ -181,41 +110,6 @@ int main(void)
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
-    // SCREEN QUAD
-    float screenQuadVertices[] = {
-        //positions     //texPos  
-        -1.0f, -1.0f,   0.0f, 0.0f,
-         1.0f, -1.0f,   1.0f, 0.0f,
-         1.0f,  1.0f,   1.0f, 1.0f,
-        -1.0f,  1.0f,   0.0f, 1.0f,
-    };
-
-    unsigned int screenQuadIndices[] = {
-        0, 1, 2, 2, 3, 0
-    };
-
-    unsigned int screenQuadVAO, screenQuadVBO, screenQuadEBO;
-    glGenVertexArrays(1, &screenQuadVAO);
-    glBindVertexArray(screenQuadVAO);
-
-    glGenBuffers(1, &screenQuadVBO);
-    glBindBuffer(GL_ARRAY_BUFFER, screenQuadVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(screenQuadVertices), screenQuadVertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
-
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
-
-    glGenBuffers(1, &screenQuadEBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, screenQuadEBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(screenQuadIndices), screenQuadIndices, GL_STATIC_DRAW);
-
-    // postFX shader
-    ResourceManager::loadShader("res/shaders/postFX.vert", "res/shaders/postFX.frag", "postFX");
-    Shader& shaderPostFX = ResourceManager::getShader("postFX");
-
     Game game{window, SCREEN_WIDTH, SCREEN_HEIGHT};
 
     glEnable(GL_BLEND);
@@ -225,37 +119,7 @@ int main(void)
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-        glBindFramebuffer(GL_FRAMEBUFFER, MSFBO);
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // WIREFRAME MODE
-
         game.render();
-
-        glBindFramebuffer(GL_READ_FRAMEBUFFER, MSFBO);
-        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, intermediaryFBO);
-
-        glBlitFramebuffer(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, GL_COLOR_BUFFER_BIT, GL_NEAREST);
-         
-        glBindFramebuffer(GL_FRAMEBUFFER, 0);
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-        shaderPostFX.use();
-        shaderPostFX.setInt("tex", 0);
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, intermediaryColorBuffer);
-
-        glBindVertexArray(screenQuadVAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0);
-
-        //ResourceManager::getShader("testShader").use();
-
-        //glBindVertexArray(gridVAO);
-        //glDrawArrays(GL_LINES, 0, numGridVertices);
-        //glBindVertexArray(0);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -266,7 +130,6 @@ int main(void)
 
     glDeleteVertexArrays(1, &gridVAO);
     glDeleteBuffers(1, &gridVBO);
-    glDeleteFramebuffers(1, &MSFBO);
 
     glfwTerminate();
     return 0;
