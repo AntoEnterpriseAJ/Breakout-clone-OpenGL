299fdc4def3e5c9fa7eb386ef83c9ca53265ca92
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-09-26 19:38:35 +0300

    game constants

--- src/include/GameConstants.h
+++ src/include/GameConstants.h
@@ -1,5 +1,18 @@
+#include "glm/glm.hpp"
 
 namespace GameConstants
 {
-    constexpr float shakeTime = 0.3f;
+   static constexpr float shakeTime       = 0.3f;
+   static constexpr float initPadSpeed    = 500.0f;
+   static constexpr float initBallRadius  = 15.0f;
+   static constexpr glm::vec2 initPadSize = {100.0f, 20.0f};
+   static constexpr glm::vec2 initPadPos  = {static_cast<float>(800) / 2.0f - initPadSize.x / 2.0f, static_cast<float>(600) - initPadSize.y};
+   static constexpr glm::vec2 initBallPos = {initPadPos.x + initPadSize.x / 2.0f - initBallRadius, initPadPos.y - initBallRadius * 2.0f};
+   static constexpr glm::vec2 ballInitVel = glm::vec2{0.0f, -350.0f};
+
+   static constexpr glm::vec2 initPowerUpVel = {0.0f, 150.0f};
+
+   static constexpr float effectSpeedUpTime = 5.0f;
+   static constexpr float effectConfuseTime = 5.0f;
+   static constexpr float effectChaosTime   = 5.0f;
 }
