c62177d3949431b77ecd550f8638be84b545c152
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-09-26 19:37:46 +0300

    PowerUp class

---
+++ src/PowerUp.cpp
@@ -0,0 +1,63 @@
+#include "PowerUp.h"
+#include "GameConstants.h"
+
+PowerUp::PowerUp()
+    : GameObject{}, m_type{}, m_duration{0.0f}, m_active{false}
+{}
+
+PowerUp::PowerUp(const Texture2D& sprite, glm::vec2 pos, glm::vec2 size, PowerUp::Type type, float duration)
+    : GameObject{sprite, pos, size}, m_type{type}, m_duration{duration}, m_active{false}
+{}
+
+PowerUp& PowerUp::operator=(const PowerUp& other)
+{
+    if (this != &other)
+    {
+        this->GameObject::operator=(other);
+        m_type     = other.m_type;
+        m_duration = other.m_duration;
+    }
+
+    return *this;
+}
+
+PowerUp::Type PowerUp::getType() const
+{
+    return m_type;
+}
+
+bool PowerUp::isActive() const
+{
+    return m_active;
+}
+
+void PowerUp::updateMove(float deltaTime, unsigned int screenHeight)
+{
+    glm::vec2 vel = GameConstants::initPowerUpVel;
+
+    if (m_position.y > screenHeight)
+    {
+        m_isDestroyed = true;
+        return;
+    }
+
+    m_position.y += vel.y * deltaTime;
+}
+
+void PowerUp::updatEffectDuration(float deltaTime)
+{
+    if (m_active)
+    {
+        m_duration -= deltaTime;
+
+        if (m_duration <= 0.0f)
+        {
+            m_active = false;
+        }
+    }
+}
+
+void PowerUp::activate()
+{
+    m_active = true;
+}
---
+++ src/include/PowerUp.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "GameObject.h"
+#include "SpriteRenderer.h"
+#include "PostProcessor.h"
+#include <memory>
+
+class PowerUp : public GameObject
+{
+public:
+    enum class Type
+    {
+        speedUp,
+        confuse,
+        chaos,
+        count,
+    };
+
+    PowerUp();
+    PowerUp(const Texture2D& sprite, glm::vec2 pos, glm::vec2 size, PowerUp::Type type, float duration);
+    PowerUp& operator=(const PowerUp& other);
+    
+    Type getType() const;
+    bool isActive() const;
+
+    void updateMove(float deltaTime, unsigned int screenHeight);
+    void updatEffectDuration(float deltaTime);
+    void activate();
+
+private:
+    Type    m_type;
+    bool    m_active;
+    float   m_duration;
+};
