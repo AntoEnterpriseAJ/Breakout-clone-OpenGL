62fd5021e9c420b1c9978061a6a364f2f7bbb5d4
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-10-01 23:03:40 +0300

    cleaned the code

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,73 +1,19 @@
-#include <iostream>
-#include "glad/glad.h"
-#include "GLFW/glfw3.h"
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb/stb_image.h"
-#include "glm/glm.hpp"
 #include "Game.h"
-#include "ResourceManager.h"
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void APIENTRY glDebugOutput(GLenum source, GLenum type, unsigned int id, GLenum severity, 
-                            GLsizei length, const char *message, const void *userParam);
+GLFWwindow* createWindow(unsigned int screenWidth, unsigned int screenHeight);
+void initOpenGL();
 
-static const unsigned int SCREEN_WIDTH = 800;
+static const unsigned int SCREEN_WIDTH  = 800;
 static const unsigned int SCREEN_HEIGHT = 600;
 
 int main(void)
 {
-    GLFWwindow* window;
-
-    if (!glfwInit())
-        return -1;
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true); 
-
-    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "BreakoutClone", nullptr, nullptr);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
-    glfwSetWindowTitle(window, "BreakoutCLone");
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "ERROR::GLAD: Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
-    else
-    {
-        int minor, major;
-        glfwGetVersion(&minor, &major, nullptr);
-        std::cout << "Succesffuly initialized GLAD\nOpenGL " << major << "." << minor << std::endl;
-    }
-
-    int flags; glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
-    if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
-    {
-        glEnable(GL_DEBUG_OUTPUT);
-        glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS); 
-        glDebugMessageCallback(glDebugOutput, nullptr);
-        glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
-        std::cout << "Succesfully initialized debug output" << std::endl;
-    }
-    else 
-        std::cout << "ERROR::DEBUG_OUTPUT: Failed to initialize debug output" << std::endl;
-
-    //glfwSwapInterval(0);
-    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+	GLFWwindow* window = createWindow(SCREEN_WIDTH, SCREEN_HEIGHT);
+    initOpenGL();
 
     Game game{window, SCREEN_WIDTH, SCREEN_HEIGHT};
-
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     while (!glfwWindowShouldClose(window))
     {
